90c04c4ff289a62619d60330ed54d42785e26238
Ubuntu <ubuntu@ip-172-31-32-233.us-east-2.compute.internal>
2021-10-27 15:25:21 +0000

    renewed with error removal

--- infra/app.py
+++ infra/app.py
@@ -16,24 +16,8 @@ from infra import infra_stack
 from infra_stack import InfraStackRizwan
 
 app = core.App()
-InfraStackRizwan(app, "InfraStackRizwan",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=core.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+InfraStackRizwan(app, "InfraStackRizwan")
     
     
-PipelineStack(app, 'PipelineStackRizwanfifth', env=core.Environment(account='315997497220', region='us-east-2'))
+PipelineStackRizwan(app, 'PipelineStackRizwansixth', env=core.Environment(account='315997497220', region='us-east-2'))
 app.synth()
