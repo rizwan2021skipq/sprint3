5a1fa4e99450fef17446f19105e09b8ef594d6bc
Ubuntu <ubuntu@ip-172-31-32-233.us-east-2.compute.internal>
2021-10-27 09:11:26 +0000

    role issues resolved

--- infra/pipeline_stack.py
+++ infra/pipeline_stack.py
@@ -2,45 +2,68 @@ from aws_cdk import core
 from aws_cdk import aws_codepipeline_actions as cpactions
 from aws_cdk import pipelines
 from infra_stage import InfraStage
+from aws_cdk.pipelines import ManualApprovalStep
+from aws_cdk import aws_iam
 
 class PipelineStack(core.Stack):
     def __init__(self,scope:core.Construct, id:str, **kwargs):
         super().__init__(scope,id, **kwargs)
         
+        cbRole = aws_iam.Role(self, "cb-role", 
+            assumed_by=aws_iam.ServicePrincipal('codebuild.amazonaws.com'), 
+            managed_policies=[  
+                                # Adding Lamda Execution
+                                aws_iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AWSLambdaBasicExecutionRole'),
+                                # Adding CloudWatch Full Access
+                                aws_iam.ManagedPolicy.from_aws_managed_policy_name('CloudWatchFullAccess'),
+                                # Adding S3 Full Access
+                                aws_iam.ManagedPolicy.from_aws_managed_policy_name('AmazonS3FullAccess'),
+                                aws_iam.ManagedPolicy.from_aws_managed_policy_name('AmazonDynamoDBFullAccess')
+                                
+                                ])
+        
+        
+        role=cbRole
+        
         source=pipelines.CodePipelineSource.git_hub(repo_string='rizwan2021skipq/sprint3', branch='main', authentication=core.SecretValue.secrets_manager('rizwan_github_token')
         , trigger=cpactions.GitHubTrigger.POLL)
         
-        synth=pipelines.ShellStep('synth',input=source, commands=["cd infra", "pip install -r requirements.txt", "npm install -g aws-cdk"
-        , "cdk synth"],primary_output_directory='infra/cdk.out')
+        synth=pipelines.CodeBuildStep('synth_rizwan', input=source, commands=["cd infra", "pip install -r requirements.txt", "npm install -g aws-cdk"
+        , "cdk synth"] ,primary_output_directory='infra/cdk.out', role= cbRole)
+        #synth=pipelines.ShellStep('synth',input=source, commands=["cd infra", "pip install -r requirements.txt", "npm install -g aws-cdk"
+        #, "cdk synth"],primary_output_directory='infra/cdk.out')
         
-        pipeline=pipelines.CodePipeline(self,'Pipeline', synth=synth)
+        pipeline=pipelines.CodePipeline(self,'PipelineRizwan', synth=synth)
         
         
         
-        beta=InfraStage(self, "Beta", env={
+        beta=InfraStage(self, "BetaRizwan", env={
             
             'account':'315997497220',
             'region':'us-east-2'
         })
         
-        gamma=InfraStage(self, "Gamma", env={
+        gamma=InfraStage(self, "GammaRizwan", env={
             
             'account':'315997497220',
             'region':'us-east-2'
         })
         
-        prod=InfraStage(self, "Production", env={
+        prod=InfraStage(self, "ProductionRizwan", env={
             
             'account':'315997497220',
             'region':'us-east-2'
         })
         
         beta_stage=pipeline.add_stage(beta)
-        beta_stage_preapproval= beta_stage.add_pre(pipelines.ManualApprovalStep('beta_approval'))
+        beta_stage_preapproval= beta_stage.add_pre(ManualApprovalStep('beta_approval_rizwan'))
         
         gamma_stage=pipeline.add_stage(gamma)
-        gamma_stage_preapproval= gamma_stage.add_pre(pipelines.ManualApprovalStep(' gamma_approval'))
+        gamma_stage_preapproval= gamma_stage.add_pre(ManualApprovalStep(' gamma_approval_rizwan'))
         
         production_stage=pipeline.add_stage(prod)
-        production_stage_preapproval= production_stage.add_pre(pipelines.ManualApprovalStep('production_approval'))
+        production_stage_preapproval= production_stage.add_pre(ManualApprovalStep('production_approval_rizwan'))
         
+        # Create a role
+
+    
