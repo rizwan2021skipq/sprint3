e0892ac854179c1c8cc7fd6ddb0a7556dd3055ef
Ubuntu <ubuntu@ip-172-31-38-90.us-east-2.compute.internal>
2021-11-02 13:13:41 +0000

    both tests r present

--- infra/lambda_folder/integ_test.py
+++ infra/lambda_folder/integ_test.py
@@ -1,7 +1,7 @@
 import pytest
 import pytest
 from aws_cdk import core
-from infra import infra_stack
+#from infra import infra_stack
 from infra_stack import InfraStackRizwan
 
 
@@ -11,8 +11,8 @@ def test_lambda():
         Test that an available website is actually available
         """
         app=core.App()
-        InfraStackRizwan(app, "testing_stack")
-        template=app.synth().get_stack_by_name("testing_stack").template
+        InfraStackRizwan(app, "testingstack")
+        template=app.synth().get_stack_by_name("testingstack").template
         functions=[ resource for resource in template['Resources'].values() if resource['Type']=='AWS::Lambda::Function']
         
         assert len(functions)==2
