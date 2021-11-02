d73c53430087dd2679de636f4492ac8a62da4229
Ubuntu <ubuntu@ip-172-31-38-90.us-east-2.compute.internal>
2021-11-02 16:06:53 +0000

    corrected requirements.txt

--- infra/infra/pipeline_stack.py
+++ infra/infra/pipeline_stack.py
@@ -81,7 +81,7 @@ class PipelineStackRizwan(core.Stack):
         pipelines.ShellStep("Approve",
             # Use the contents of the 'integ' directory from the synth step as the input
             #input=synth.add_output_directory("integ"),
-            commands=["cd infra", "cd lambda_folder", "pytest unit_tests.py", "pytest integ_test.py"]
+            commands=["cd infra", "pip install -r requirements.txt", "cd lambda_folder", "pytest unit_tests", "pytest integ_test"]
                             )
             ])
         
