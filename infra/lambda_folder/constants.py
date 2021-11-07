5000bf7a2b538389faed09961d560d6943c0279c
Ubuntu <ubuntu@ip-172-31-38-90.us-east-2.compute.internal>
2021-11-07 07:57:02 +0000

    addedd errors alrms

--- infra/lambda_folder/constants.py
+++ infra/lambda_folder/constants.py
@@ -12,6 +12,7 @@ THRESHOLD_OF_AVAILABILITY=1
 # Constant defining threshold of latency for all websites
 THRESHOLD_OF_LATENCY=0.100
 THRESHOLD_OF_DURATION=0.1E-3
+THRESHOLD_OF_ERRORS=1
 EMAIL="rizwan.s@skipq.org"
 LAMBDA_ARN="arn:aws:lambda:us-east-2:315997497220:function:InfraStackRizwan-loggerlambda7BA3B318-XCPO1vGRhJD4"
 
@@ -20,4 +21,6 @@ TOPIC_NAME='alarms_testing'
 BUCKET_NAME="rizwanbucket2021"
 FILE_IN_BUCKET='website_data.json'
 TABLE_NAME="NEWS"
+
+
 
