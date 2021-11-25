84b7471060eb6b60d73d9a1fa3ce10c5fe9655bd
Ubuntu <ubuntu@ip-172-31-38-90.us-east-2.compute.internal>
2021-11-25 16:26:59 +0000

    replaced expired github token

--- infra/infra/pipeline_stack.py
+++ infra/infra/pipeline_stack.py
@@ -48,7 +48,7 @@ class PipelineStackRizwan(core.Stack):
         
 
         # Source Step
-        source=pipelines.CodePipelineSource.git_hub(repo_string='rizwan2021skipq/sprint3', branch='main', authentication=core.SecretValue.secrets_manager('rizwan_github_token')
+        source=pipelines.CodePipelineSource.git_hub(repo_string='rizwan2021skipq/sprint3', branch='main', authentication=core.SecretValue.secrets_manager('new_rizwan_github_token')
         , trigger=cpactions.GitHubTrigger.POLL)
         
         # Build Step
